5857820f33829bea203cc37fc4f98b6ce8e36e03
lopezjoa <137831857+lopezjoa@users.noreply.github.com>
2023-06-28 15:29:55 -0500

    Update add_topics.py

--- add_topics.py
+++ add_topics.py
@@ -71,7 +71,7 @@ def get_repo_and_topics_from_args(repos_dict):
         if repo_key in repos_dict:  # check if repo_key exists as a key in repos_dict
             for topic in i_list:
                 if topic != "":  # check if the topic is not empty
-                    repos_dict[repo_key][.append(topic) # add topics to repo name dictionary if it's in repo dictionary
+                    repos_dict[repo_key][1].append(topic) # add topics to repo name dictionary if it's in repo dictionary
                     print("Topics: ", repos_dict[repo_key][1])
                 else:
                     print("Error: At least one topic must be provided.")
